8a52ccf4e17e2c0feae15fb6522da5a4d64bfe66
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-04-25 08:31:03 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuotesApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
